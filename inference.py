ac227150ce94e9e7d80a8dab39accc04d3e9c302
Shahir-codes <shahir55312@gmail.com>
2026-04-10 20:51:22 +0530

    Update inference.py

---
+++ inference.py
@@ -0,0 +1,21 @@
+import requests
+
+BASE_URL = "http://127.0.0.1:8000"
+
+
+res = requests.get(f"{BASE_URL}/reset")
+data = res.json()
+state = data["state"]
+
+print("Initial State:", state)
+
+
+action = 1  
+
+res = requests.post(f"{BASE_URL}/step", params={"action": action})
+result = res.json()
+
+print("Action:", action)
+print("Next State:", result["state"])
+print("Reward:", result["reward"])
+print("Done:", result["done"])
